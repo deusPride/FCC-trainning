18dc8b2a957a52c1d385a70fd65d9ce4c5d97004
deuspride <sinpride9@gmail.com>
2025-01-22 07:48:27 +0100

    Urls Shortener submission

--- [Project 3]-URL Shortener Microservice/index.js
+++ [Project 3]-URL Shortener Microservice/index.js
@@ -2,23 +2,87 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyParser = require('body-parser');
+const dns = require('dns');
+const mongoose = require('mongoose');
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/public', express.static(`${process.cwd()}/public`));
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
+const mongoURI = process.env.MONGO_URI;
 
-app.use(cors());
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+const ShortSchema = {
+  original_url: String,
+  short_url: String
+};
+const Short = mongoose.model('Short', ShortSchema);
+
+const createAndSaveShort = (url, done) => {
+  const short = new Short({ original_url: url, short_url: Math.floor(Math.random() * 1000) });
+  short.save((err, data) => {
+    if (err) return console.error(err);
+    done(null, data);
+  });
+};
+
+const findShortByOriginalUrl = (url, done) => {
+  Short.findOne({ original_url: url }, (err, shortFound) => {
+    if (err) return console.error("Error:", err);
+    done(null, shortFound);
+  });
+};
+
+const findShortByShortUrl = (shortUrl, done) => {
+  Short.findOne({ short_url: shortUrl }, (err, shortFound) => {
+    if (err) return console.error("Error:", err);
+    done(null, shortFound);
+  });
+};
+
+app.get('/', function (req, res) {
+  res.sendFile(`${process.cwd()}/views/index.html`);
+});
+app.get('/api/test/', function (req, res) {
+  res.json({test : 254});
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.post('/api/shorturl/', function (req, res) {
+  const url = req.body.url;
+  const urlWithoutHttps = url.split('/')[2];
+  dns.lookup(urlWithoutHttps, (err) => {
+    if (err) {
+      return res.json({ error: 'invalid url' });
+    }
+    findShortByOriginalUrl(url, (err, data) => {
+      if (err) return console.error(err);
+      if (data) {
+        return res.json({ original_url: data.original_url, short_url: parseInt(data.short_url) });
+      }
+      createAndSaveShort(url, (err, data) => {
+        if (err) return console.error(err);
+        return res.json({ original_url: data.original_url, short_url: parseInt(data.short_url) });
+      });
+    });
+  });
 });
 
-app.listen(port, function() {
+app.get('/api/shorturl/:short_url', function (req, res) {
+  const shortUrl = req.params.short_url;
+  findShortByShortUrl(shortUrl, (err, data) => {
+    if (err) return console.error(err);
+    if (data) {
+      return res.redirect(data.original_url);
+    }
+    return res.json({ error: 'No short url found for given input' });
+  });
+});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
--- [Project 4]-Exercise Tracker/index.js
+++ [Project 4]-Exercise Tracker/index.js
@@ -1,17 +1,145 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
 require('dotenv').config()
+const mongoose = require('mongoose')
 
+
+
+//connecting to mongoDB
+const connectToMongoDB = async () => {
+  try {
+    // Connexion à MongoDB
+    await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+    console.log('Connexion à MongoDB réussie !');
+  } catch (error) {
+    // Gestion des erreurs
+    console.error('Échec de la connexion à MongoDB :', error.message);
+  }
+};
+connectToMongoDB();
+
+//Setting the middlewares
+app.use(bodyParser.urlencoded({extended: false}))
 app.use(cors())
 app.use(express.static('public'))
+
+//Launching the main view
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
 
+//Creating the Schemas
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+});
+
+const exerciceSchema = new mongoose.Schema({
+  username:String,
+  description : String,
+  duration: Number,
+  date : Date,
+});
 
 
+//Creating the Models
+const User = mongoose.model('User', userSchema);
+const Exercise = mongoose.model('Exercise', exerciceSchema);
+
+//Creating actions on my data
+const createAndSaveUser = function(user,done){
+  user = new User({username: user});
+  user.save((err, data) => {
+    if(err) return console.error(err);
+    done(null, data);
+  });
+}
+
+const findOneUserByID = async function(userId,done){
+ await User.findById(userId,(err,userFound) => {
+    if(err) return console.error("Error: ",err);
+    done(null,userFound);
+  });
+};
+
+
+const createAndSaveExercice = async function(exo,userId,done){
+  var user = {};
+  await findOneUserByID(userId,(err,userFound) => {
+    if (err) return console.error('User not Found ',err);
+    if (userFound){
+    user = userFound;
+    };
+  });
+  exo = new Exercise({
+    username :user.username,
+    description: exo.description,
+    duration: exo.duration,
+    date: exo.date
+  });
+  exo.save((err, data) => {
+    if(err) return console.error(err);
+    done(null, data);
+  });
+};
+
+
+const findAllUser =(done) => {
+  User.find({},(err,usersFound) => {
+    if (err) return console.log("Error: ",err);
+    done(null,usersFound); 
+  });  
+};
+const findExerciseByUsername = (usernamedone) => {
+  Exercise.find({username :username},(err,exercicesFound) => {
+    if (err) return console.error("Error: ",err);
+    done(null,exercicesFound);  
+  }); 
+};
+
+
+//Routes for database querying
+
+//registering an user
+app.post('/api/users',(req,res) => {
+  const user = req.body.username;
+  createAndSaveUser(user,(err,data) => {
+    if (err) return console.error('Error',err,' while creating user');
+    if(data) return res.json(data);
+  });
+});
+
+//registering an exercice
+app.post('/api/users/:_id/exercises',(req,res) => {
+  const userId = req.params._id;
+  const exo = {
+    description: req.body.description,
+    duration: parseInt(req.body.duration), 
+    date: req.body.date};
+  createAndSaveExercice(exo,userId,(err,data) => {
+    if (err) return console.error('Error',err,' while creating Exercise');
+    if(data){ 
+      const output = {
+        _id:userId,
+        username:data.username,
+        exercise:exo
+      }
+      return res.json(output)};
+  });
+});
+
+//Fetching data
+app.get('/api/users',(req,res) => {
+  const userId=req.params.id;
+  findAllUser((err,userFound) => {
+    if(err) console.log('Error: ', err);
+    if(userFound) return res.json(userFound);
+    
+  })
+  
+})
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
